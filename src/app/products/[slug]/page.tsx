f26bb06ad8daea66c4b776b041b37dde7db0ad3c
NOHHYUNGBIN <shgudqlsz@naver.com>
2025-03-27 22:54:09 +0900

    다이나믹 라우트 페이지를 SSG로 만들기

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -5,3 +5,10 @@ export default async function PantsPage({ params }: Props) {
   const { slug } = await params;
   return <h1>{slug}페이지~</h1>;
 }
+
+//다이나믹 라우트에서 특정한 경로의 페이지를 미리만들어 놓기위하여..
+//generateStaticParams next에서 정해놓은 이름의 함수를 사용해야함.
+export function generateStaticParams() {
+  const products = ["pants", "skirt"];
+  return products.map((product) => ({ slug: product }));
+}
