a1d367b5495779fb08a0d1dfe541605a63aad0b6
NOHHYUNGBIN <shgudqlsz@naver.com>
2025-03-29 21:14:52 +0900

    layout사용

---
+++ src/app/products/layout.tsx
@@ -0,0 +1,16 @@
+import styles from "./layout.module.css";
+export default function ProductsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <nav className={styles.nav}>
+        <a>여성옷</a>
+        <a>남성옷</a>
+      </nav>
+      <section className={styles.product}>{children}</section>
+    </>
+  );
+}
