6e3bfcccdf6f57f486c7895aa117a1a39378cd0e
NOHHYUNGBIN <shgudqlsz@naver.com>
2025-03-31 23:02:03 +0900

    로딩+SSG,SSR,ISR 확인

--- src/api/products.ts
+++ src/api/products.ts
@@ -8,8 +8,7 @@ export type Product = {
 };
 
 export async function getProducts(): Promise<Product[]> {
-  for (let i = 0; i > 1000000000000000; i++) {}
-  console.debug("ㅎㅎㅇ");
+  await new Promise((r) => setTimeout(r, 3000));
   const filePath = path.join(process.cwd(), "data", "products.json");
   const data = await fs.readFile(filePath, "utf-8");
   return JSON.parse(data);
